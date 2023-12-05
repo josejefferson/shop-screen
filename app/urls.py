c064824f344597a1926b60078e8b0c4982e0b0b5
Marcos Paulo <marcosplira12@gmail.com>
2023-12-05 20:56:51 -0300

    feat: Adicionado página de pesquisa

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.pagina_inicial, name='pagina_inicial'),
     path('anuncios', views.anuncios, name='anuncios'),
+    path('pesquisa', views.pesquisa, name='pesquisa'),
+    path('pesquisar', views.resultados_pesquisa, name='resultados_pesquisa'),
+    path('produto/<int:id>', views.produto, name='produto'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from .models import Produto, Anuncio
 
 def pagina_inicial(request):
-    produtos = Produto.objects.all()
-    return render(request, 'index.html', {'produtos': produtos})
+    return render(request, 'index.html')
 
 def anuncios(request):
     anuncios = Anuncio.objects.all()
     return render(request, 'anuncios.html', {'anuncios': anuncios})
+
+def pesquisa(request):
+    return render(request, 'pesquisa.html')
+
+def resultados_pesquisa(request):
+    pesquisa = request.GET['q']
+    return render(request, 'resultados_pesquisa.html', {'pesquisa':pesquisa})
+
+def produto(request, id):
+    return render(request, 'produto.html')
