227f0070b3fcd6dda58cc0b5e346874d7070146e
Marcos Paulo <marcosplira12@gmail.com>
2023-12-05 22:50:30 -0300

    feat: filtrando pelo nome e pegando pelo id

--- app/views.py
+++ app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Produto, Anuncio
 
 def pagina_inicial(request):
+    """
+    Pagina inicial
+
+    """
     return render(request, 'index.html')
 
 def anuncios(request):
@@ -12,8 +16,11 @@ def pesquisa(request):
     return render(request, 'pesquisa.html')
 
 def resultados_pesquisa(request):
-    pesquisa = request.GET['q']
-    return render(request, 'resultados_pesquisa.html', {'pesquisa':pesquisa})
+    pesquisa = request.GET.get('q', '')
+    filterNome = Produto.objects.filter(nome__icontains=pesquisa)
+
+    return render(request, 'resultados_pesquisa.html', {'pesquisa':pesquisa, 'filterNome':filterNome})
 
 def produto(request, id):
-    return render(request, 'produto.html')
+    p = Produto.objects.get(id=id)
+    return render(request, 'produto.html', {'id':p})
